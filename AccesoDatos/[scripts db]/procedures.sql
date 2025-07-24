3283c0f1403df74c11aefa93bc14f4d5a051202b
Salchy <leandrocg524@gmail.com>
2025-07-24 10:36:51 -0300

    Transformación de PROCEDURES getTicketInfo y GetCommits a VISTAS

--- AccesoDatos/[scripts db]/procedures.sql
+++ AccesoDatos/[scripts db]/procedures.sql
@@ -22,9 +22,6 @@ EXEC SP_CrearUsuario 'jlopez', 'Julieta Lopez', 'jlopez@altaplastica.com.ar', 'a
 EXEC SP_CrearUsuario 'mgarcia', 'Marcos Garcia', 'mgarcia@altaplastica.com.ar', 'marcos2025', 1;
 EXEC SP_CrearUsuario 'cortega', 'Camila Ortega', 'cortega@altaplastica.com.ar', 'camila#789', 1;
 
-
-SELECT * FROM Usuarios;
- 
 CREATE PROCEDURE SP_CrearTicket (
 	@owner INT,
 	@title VARCHAR(75),
@@ -37,24 +34,11 @@ AS BEGIN
 END
 
 SELECT * FROM Tickets;
-
-SELECT * FROM EstadosTicket;
-
+ 
 EXEC SP_CrearTicket 3, 'Incidencias / Errores - Problemas de hardware', 'El equipo presenta fallas de encendido y sobrecalentamiento. Se requiere revisión del hardware interno y posible reemplazo de componentes defectuosos';
 EXEC SP_CrearTicket 5, 'Consultas / Dudas - Asesoramiento técnico', 'Hola, tengo una duda sobre cómo acceder de forma remota al sistema de gestión desde fuera de la oficina. ¿Podrían indicarme si necesito instalar algo adicional o si hay un procedimiento específico que deba seguir? Gracias';
 EXEC SP_CrearTicket 4, 'Implementaciones / Proyectos - Actualizaciones de sistemas', 'Buenas, quería consultar si ya está disponible la nueva versión del sistema de facturación que mencionaron la semana pasada. Necesitamos saber si requiere alguna acción de nuestra parte para la actualización o si se hará de forma automática. También nos gustaría saber qué mejoras incluye';
 EXEC SP_CrearTicket 2, 'Seguridad Informática - Revisión de logs / auditorías', 'Revisión de logs de VMware';
-
-
-CREATE PROCEDURE SP_GetTicketInfo (
-	@idTicket INT
-)
-AS BEGIN
-	SELECT T.Id, U.Id AS 'IdUsuarioCreador', U.Nombre, U.TipoUsuario, U.Correo, T.Titulo, T.IdEstado, ET.NombreEstado, T.FechaCreacion, T.FechaActualizacion, T.Descripcion FROM Tickets AS T
-	INNER JOIN Usuarios AS U ON T.IdUsuarioCreador = U.Id
-	INNER JOIN EstadosTicket AS ET ON T.IdEstado = ET.Id
-	WHERE T.Id = @idTicket;
-END
 
 CREATE PROCEDURE SP_RegistrarCommit (
 	@typeCommit INT,
@@ -67,12 +51,3 @@ AS BEGIN
 	OUTPUT inserted.Id
 	VALUES (@typeCommit, @idTicket, @idAutor, @message);
 END
-
-EXEC SP_RegistrarCommit 1, 3, 4, 'Este sería una respuesta del dueño del ticket';
-SELECT * FROM Usuarios;
-
-SELECT * FROM Tickets;
-
-SELECT * FROM Commits;
-
-UPDATE Commits SET TipoCommit = 1 Where Id = 3;
--- AccesoDatos/[scripts db]/vistas.sql
+++ AccesoDatos/[scripts db]/vistas.sql
@@ -6,5 +6,17 @@ CREATE VIEW VW_GetAllTickets AS
 	FROM Tickets AS T
 	INNER JOIN Usuarios AS U ON T.IdUsuarioCreador = U.Id
 	INNER JOIN EstadosTicket AS ET ON T.IdEstado = ET.Id
+GO;
 
-SELECT * FROM VW_GetAllTickets
+CREATE VIEW VW_GetTicketInfo AS
+	SELECT
+		T.Id, U.Id AS 'IdUsuarioCreador', U.Nombre, U.TipoUsuario, U.Correo, T.Titulo, T.IdEstado, ET.NombreEstado, T.FechaCreacion, T.FechaActualizacion, T.Descripcion
+	FROM Tickets AS T
+	INNER JOIN Usuarios AS U ON T.IdUsuarioCreador = U.Id
+	INNER JOIN EstadosTicket AS ET ON T.IdEstado = ET.Id
+
+CREATE VIEW VW_GetCommits AS
+	SELECT
+		C.Id, C.TipoCommit, C.IdTicketRelacionado, C.IdAutor, U.Nombre, C.Fecha, C.Mensaje
+	FROM Commits AS C
+	INNER JOIN Usuarios AS U ON C.IdAutor = U.Id

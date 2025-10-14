51721d3c571e98c06d37e17993ab7bac7efd70aa
Salchy <leandrocg524@gmail.com>
2025-10-14 13:17:18 -0300

    Feat: Helper para notificaciones en masa de correos

---
+++ AplicacionWeb/Helpers/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AccesoDatos;
+using DTO;
+
+namespace AplicacionWeb.Helpers
+{
+    public static class Helper
+    {
+        public static void notificarSupporters(int ticketID, string asunto, string msg)
+        {
+            UsuarioDatos usuarioDatos = new UsuarioDatos();
+
+            try
+            {
+                foreach (UsuarioDTO supporter in usuarioDatos.GetSupporters())
+                {
+                    MailHelper.SendEmail(supporter.Correo, asunto, msg, supporter.Nombre, ticketID.ToString());
+                }
+            }
+            catch (Exception Ex)
+            {
+                throw Ex;
+            }
+        }
+    }
+}
